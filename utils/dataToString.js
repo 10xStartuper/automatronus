3ecfd55c07aa19b5f7ffcae8102d9c68aad73ea4
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-10-17 23:49:51 +0500

    utils/dataToString is working

---
+++ utils/dataToString.js
@@ -0,0 +1,18 @@
+const dataToString = (data) => {
+  let newData = "";
+  data.forEach((item) => {
+    newData += `<b>Message:</b> ${item.message}
+    \n<b>Time:</b> ${item.time}
+    \n<b>Repeat:</b>`;
+
+    for (const [key, value] of Object.entries(item.weekdays)) {
+      //   console.log(`${key}: ${value}`);
+      //   console.log(value);
+      newData += `\n<b>${key}:</b> ${value ? "✅" : "❌"}`;
+    }
+    newData += `\n----------------\n`;
+  });
+  return newData;
+};
+
+module.exports = dataToString;
